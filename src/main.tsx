11ec476a5a3afe0419ef45ce613c0a4bc64bd101
Danish <danishrpatechnologies@gmail.com>
2025-12-29 18:05:32 +0530

    scroll fixed

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,12 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import { BrowserRouter } from "react-router-dom";
+import ScrollToTop from "./scroll-top.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
+      <ScrollToTop />
       <App />
     </BrowserRouter>
   </StrictMode>
